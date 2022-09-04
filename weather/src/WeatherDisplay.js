c2a4426f0ad8ad51f9ba1e5c74da43f95b55e4c5
muhammad gmal ezzat <59686905+muhammadgmalezzat@users.noreply.github.com>
2022-09-04 02:10:48 +0200

    Weather Display function implemented

--- weather/src/WeatherDisplay.js
+++ weather/src/WeatherDisplay.js
@@ -1,9 +1,27 @@
 //import react , react dom
 import React from 'react'
 
-const weatherDisplay = () => {
+
+
+
+const getSeason = (lat, month )=> {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const weatherDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burr, it is Chilly' : 'Lets hit the Beach';
+    const iconName = season === 'winter' ? 'sonwflake' : 'sun';
     return (
-        <div>season</div>
+        <div>
+            <i className={`${iconName} icon `}></i>
+            <h1>{text}</h1>
+            <i className={`${iconName} icon `}></i>
+        </div>
     );
 };
 
--- weather/src/index.js
+++ weather/src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import WeatherDisplay from './WeatherDisplay'
+import WeatherDisplay from './WeatherDisplay'
 
 
 // Create class components
@@ -12,28 +12,19 @@ class App extends React.Component {
         //when we define constructor function in app component ,we are essentially overriding or replacing constructor function in react component ,
         //so to make sure that constructor function in react component is called we use super to call parents
         //every time we change state it will call rener method 
-    
-    constructor(props){
-        super(props);
-        this.state = { lat: null, errorMessage: "" };
-        
+    state ={ lat: null, errorMessage: ""}
+
+    //creating component did method 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                //to update state we should use setstate function 
-                this.setState({ lat: position.coords.latitude })
-                //we should never use this
-                //this.State.lat = position.coords.latitude;
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
-        );
-    };
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message }));
+        }
     //React says that we have to define render function or it will through error message 
     render() {
         if (this.state.lat && !this.state.errorMessage) 
         { 
-            return (<div>lat : {this.state.lat}</div>)
+            return <WeatherDisplay lat={this.state.lat }/>
         }
         if (!this.state.lat && this.state.errorMessage)
         {
